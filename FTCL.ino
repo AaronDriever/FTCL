2d5d2fb3dbff317194e8d2bd559eea4026414a73
AaronDriever <aarondriever@gmail.com>
2014-07-01 23:24:38 -0700

    Added functions

--- FTCL.ino
+++ FTCL.ino
@@ -2,47 +2,100 @@
 FTCL
 7/1/14
 ********************************************************/
+
+
+
+
+
+
+#include "FastLED.h"
 #include <Adafruit_NeoPixel.h>
 #include "WS2812_Definitions.h"
+
+
+
+// How many leds are in the strip?
+#define NUM_LEDS 4
+
+// Data pin that led data will be written out over
+#define PIN 3
+
+// Clock pin only needed for SPI based chipsets when not using hardware SPI
+//#define CLOCK_PIN 8
+
 
 #define PIN 6
 #define LED_COUNT 5
 
-// Parameter 1 = number of pixels in leds
+
+// This is an array of leds.  One item for each led in your strip.
+CRGB leds[LED_COUNT];
+// Parameter 1 = number of pixels in pixel
 // Parameter 2 = pin number (most are valid)
 // Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 pixel)
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel leds = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixel = Adafruit_NeoPixel(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() 
 {
-	leds.begin();  // Call this to start up the LED strip.
-	clearLEDs();   // This function, defined below, turns all LEDs off...
-	leds.show();   // ...but the LEDs don't actually update until you call this.
+	// sanity check delay - allows reprogramming if accidently blowing power w/leds
+	delay(2000);
+
+	// Uncomment one of the following lines for your leds arrangement.
+	// FastLED.addLeds<TM1803, PIN, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<TM1804, PIN, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<TM1809, PIN, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<WS2811, PIN, RGB>(leds, NUM_LEDS);
+	//FastLED.addLeds<WS2812, PIN, RGB>(leds, NUM_LEDS);
+	//FastLED.addLeds<WS2812B, PIN, RGB>(leds, NUM_LEDS);
+	FastLED.addLeds<NEOPIXEL, PIN, GRB>(leds, NUM_LEDS);
+	// FastLED.addLeds<WS2811_400, PIN, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<UCS1903, PIN, RGB>(leds, NUM_LEDS);
+
+	// FastLED.addLeds<WS2801, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<SM16716, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<LPD8806, RGB>(leds, NUM_LEDS);
+
+	// FastLED.addLeds<WS2801, PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<SM16716, PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+	// FastLED.addLeds<LPD8806, PIN, CLOCK_PIN, RGB>(leds, NUM_LEDS);
+	pixel.begin();  // Call this to start up the LED strip.
+	clearpixel();   // This function, defined below, turns all pixel off...
+	pixel.show();   // ...but the pixel don't actually update until you call this.
 }
 
 void loop() {
 	// Some example procedures showing how to display to the pixels:
-	colorWipe(leds.Color(255, 0, 0), 50); // Red
-	colorWipe(leds.Color(0, 255, 0), 50); // Green
-	colorWipe(leds.Color(0, 0, 255), 50); // Blue
+	colorWipe(pixel.Color(255, 0, 0), 50); // Red
+	colorWipe(pixel.Color(0, 255, 0), 50); // Green
+	colorWipe(pixel.Color(0, 0, 255), 50); // Blue
 	rainbow(20);
 	rainbowCycle(20);
+	//firstLight();
+	//blink();
+	cylon2();
+	//calibrate();
+	//allLEDS();
+	//siren();
 }
 
-// Fill the dots one after the other with a color
+// Fill the dots one after the other with a color	
+//colorWipe(pixel.Color(255, 0, 0), 50); // Red
+//colorWipe(pixel.Color(0, 255, 0), 50); // Green
+//colorWipe(pixel.Color(0, 0, 255), 50); // Blue
 void colorWipe(uint32_t c, uint8_t wait) 
 {
-	for(uint16_t i=0; i<leds.numPixels(); i++) 
+	for(uint16_t i=0; i<pixel.numPixels(); i++) 
 	{
-		leds.setPixelColor(i, c);
-		leds.show();
+		pixel.setPixelColor(i, c);
+		pixel.show();
 		delay(wait);
 	}
 }
+
 
 void rainbow(uint8_t wait) 
 {
@@ -50,11 +103,11 @@ void rainbow(uint8_t wait)
 
 	for(j=0; j<256; j++) 
 	{
-		for(i=0; i<leds.numPixels(); i++) 
+		for(i=0; i<pixel.numPixels(); i++) 
 		{
-			leds.setPixelColor(i, Wheel((i+j) & 255));
+			pixel.setPixelColor(i, Wheel((i+j) & 255));
 		}
-		leds.show();
+		pixel.show();
 		delay(wait);
 	}
 }
@@ -66,11 +119,11 @@ void rainbowCycle(uint8_t wait)
 
 	for(j=0; j<256*5; j++) 
 	{ // 5 cycles of all colors on wheel
-		for(i=0; i< leds.numPixels(); i++)
+		for(i=0; i< pixel.numPixels(); i++)
 		{
-			leds.setPixelColor(i, Wheel(((i * 256 / leds.numPixels()) + j) & 255));
+			pixel.setPixelColor(i, Wheel(((i * 256 / pixel.numPixels()) + j) & 255));
 		}
-		leds.show();
+		pixel.show();
 		delay(wait);
 	}
 }
@@ -90,34 +143,34 @@ void cylon(unsigned long color, byte wait)
 	// Start at closest LED, and move to the outside
 	for (int i=0; i<=LED_COUNT-1; i++)
 	{
-		clearLEDs();
-		leds.setPixelColor(i, red, green, blue);  // Set the bright middle eye
+		clearpixel();
+		pixel.setPixelColor(i, red, green, blue);  // Set the bright middle eye
 		// Now set two eyes to each side to get progressively dimmer
 		for (int j=1; j<3; j++)
 		{
 			if (i-j >= 0)
-				leds.setPixelColor(i-j, red/(weight*j), green/(weight*j), blue/(weight*j));
+				pixel.setPixelColor(i-j, red/(weight*j), green/(weight*j), blue/(weight*j));
 			if (i-j <= LED_COUNT)
-				leds.setPixelColor(i+j, red/(weight*j), green/(weight*j), blue/(weight*j));
+				pixel.setPixelColor(i+j, red/(weight*j), green/(weight*j), blue/(weight*j));
 		}
-		leds.show();  // Turn the LEDs on
+		pixel.show();  // Turn the pixel on
 		delay(wait);  // Delay for visibility
 	}
 
 	// Now we go back to where we came. Do the same thing.
 	for (int i=LED_COUNT-2; i>=1; i--)
 	{
-		clearLEDs();
-		leds.setPixelColor(i, red, green, blue);
+		clearpixel();
+		pixel.setPixelColor(i, red, green, blue);
 		for (int j=1; j<3; j++)
 		{
 			if (i-j >= 0)
-				leds.setPixelColor(i-j, red/(weight*j), green/(weight*j), blue/(weight*j));
+				pixel.setPixelColor(i-j, red/(weight*j), green/(weight*j), blue/(weight*j));
 			if (i-j <= LED_COUNT)
-				leds.setPixelColor(i+j, red/(weight*j), green/(weight*j), blue/(weight*j));
+				pixel.setPixelColor(i+j, red/(weight*j), green/(weight*j), blue/(weight*j));
 		}
 
-		leds.show();
+		pixel.show();
 		delay(wait);
 	}
 }
@@ -129,9 +182,9 @@ void cascade(unsigned long color, byte direction, byte wait)
 	{
 		for (int i=0; i<LED_COUNT; i++)
 		{
-			clearLEDs();  // Turn off all LEDs
-			leds.setPixelColor(i, color);  // Set just this one
-			leds.show();
+			clearpixel();  // Turn off all pixel
+			pixel.setPixelColor(i, color);  // Set just this one
+			pixel.show();
 			delay(wait);
 		}
 	}
@@ -139,21 +192,21 @@ void cascade(unsigned long color, byte direction, byte wait)
 	{
 		for (int i=LED_COUNT-1; i>=0; i--)
 		{
-			clearLEDs();
-			leds.setPixelColor(i, color);
-			leds.show();
+			clearpixel();
+			pixel.setPixelColor(i, color);
+			pixel.show();
 			delay(wait);
 		}
 	}
 }
 
-// Sets all LEDs to off, but DOES NOT update the display;
-// call leds.show() to actually turn them off after this.
-void clearLEDs()
+// Sets all pixel to off, but DOES NOT update the display;
+// call pixel.show() to actually turn them off after this.
+void clearpixel()
 {
 	for (int i=0; i<LED_COUNT; i++)
 	{
-		leds.setPixelColor(i, 0);
+		pixel.setPixelColor(i, 0);
 	}
 }
 
@@ -171,10 +224,10 @@ void rainbow(byte startPosition)
 	{
 		// There are 192 total colors we can get out of the rainbowOrder function.
 		// It'll return a color between red->orange->green->...->violet for 0-191.
-		leds.setPixelColor(i, rainbowOrder((rainbowScale * (i + startPosition)) % 192));
+		pixel.setPixelColor(i, rainbowOrder((rainbowScale * (i + startPosition)) % 192));
 	}
-	// Finally, actually turn the LEDs on:
-	leds.show();
+	// Finally, actually turn the pixel on:
+	pixel.show();
 }
 
 // Input a value 0 to 191 to get a color value.
@@ -185,31 +238,141 @@ uint32_t rainbowOrder(byte position)
 	// 6 total zones of color change:
 	if (position < 31)  // Red -> Yellow (Red = FF, blue = 0, green goes 00-FF)
 	{
-		return leds.Color(0xFF, position * 8, 0);
+		return pixel.Color(0xFF, position * 8, 0);
 	}
 	else if (position < 63)  // Yellow -> Green (Green = FF, blue = 0, red goes FF->00)
 	{
 		position -= 31;
-		return leds.Color(0xFF - position * 8, 0xFF, 0);
+		return pixel.Color(0xFF - position * 8, 0xFF, 0);
 	}
 	else if (position < 95)  // Green->Aqua (Green = FF, red = 0, blue goes 00->FF)
 	{
 		position -= 63;
-		return leds.Color(0, 0xFF, position * 8);
+		return pixel.Color(0, 0xFF, position * 8);
 	}
 	else if (position < 127)  // Aqua->Blue (Blue = FF, red = 0, green goes FF->00)
 	{
 		position -= 95;
-		return leds.Color(0, 0xFF - position * 8, 0xFF);
+		return pixel.Color(0, 0xFF - position * 8, 0xFF);
 	}
 	else if (position < 159)  // Blue->Fuchsia (Blue = FF, green = 0, red goes 00->FF)
 	{
 		position -= 127;
-		return leds.Color(position * 8, 0, 0xFF);
+		return pixel.Color(position * 8, 0, 0xFF);
 	}
 	else  //160 <position< 191   Fuchsia->Red (Red = FF, green = 0, blue goes FF->00)
 	{
 		position -= 159;
-		return leds.Color(0xFF, 0x00, 0xFF - position * 8);
+		return pixel.Color(0xFF, 0x00, 0xFF - position * 8);
+	}
+}
+void firstLight()
+{
+	// Move a single white led 
+	for(int whiteLed = 0; whiteLed < NUM_LEDS; whiteLed = whiteLed + 1) 
+	{
+		// Turn our current led on to white, then show the leds
+		leds[whiteLed] = CRGB::White;
+
+		// Show the leds (only one of which is set to white, from above)
+		FastLED.show();
+
+		// Wait a little bit
+		delay(100);
+
+		// Turn our current led back to black for the next loop around
+		leds[whiteLed] = CRGB::Black;
+	}
+}
+
+void blink()
+{
+	// Turn the LED on, then pause
+	leds[0] = CRGB::Blue;
+	FastLED.show();
+	delay(500);
+	// Now turn the LED off, then pause
+	leds[0] = CRGB::Black;
+	FastLED.show();
+	delay(500);
+
+}
+void cylon2()
+{
+	// First slide the led in one direction
+	for(int i = 0; i < NUM_LEDS; i++) 
+	{
+		// Set the i'th led to red 
+		leds[i] = CRGB::DarkRed;
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		leds[i] = CRGB::Black;
+		// Wait a little bit before we loop around and do it again
+		delay(30);
+	}
+
+	// Now go in the other direction.  
+	for(int i = NUM_LEDS-1; i >= 0; i--) 
+	{
+		// Set the i'th led to red 
+		leds[i] = CRGB::DarkRed;
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		leds[i] = CRGB::Black;
+		// Wait a little bit before we loop around and do it again
+		delay(30);
+	}
+}
+void calibrate()
+{
+	leds[0] = CRGB::Thistle; 
+	leds[1] = CRGB::Green;
+	leds[2] = CRGB::Green;
+	leds[3] = CRGB::Blue;
+	leds[4] = CRGB::Blue;
+	leds[5] = CRGB::Blue;
+	FastLED.show();
+	delay(1000);
+}
+
+void allLEDS()
+{
+
+	leds[0] = CRGB::MediumBlue; 
+	leds[1] = CRGB::SteelBlue;
+	leds[2] = CRGB::Aquamarine;
+	leds[3] = CRGB::DarkViolet;
+	FastLED.show();
+	delay(1000);
+}
+
+void siren()
+{
+	// First slide the led in one direction
+	for(int i = 0; i < NUM_LEDS; i++) 
+	{
+		// Set the i'th led to red 
+		leds[i] = CRGB::Blue;
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		leds[i] = CRGB::Black;
+		// Wait a little bit before we loop around and do it again
+		delay(30);
+	}
+
+	// Now go in the other direction.  
+	for(int i = NUM_LEDS-1; i >= 0; i--) 
+	{
+		// Set the i'th led to red 
+		leds[i] = CRGB::Red;
+		// Show the leds
+		FastLED.show();
+		// now that we've shown the leds, reset the i'th led to black
+		leds[i] = CRGB::Black;
+		// Wait a little bit before we loop around and do it again
+		delay(30);
 	}
 }
--- Visual Micro/.FTCL.vsarduino.h
+++ Visual Micro/.FTCL.vsarduino.h
@@ -5,18 +5,18 @@
 	        all non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 	        note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: SparkFun Fio V3 3.3V/8MHz, Platform=avr, Package=SF32u4_boards-master
+	Hardware: Arduino Uno, Platform=avr, Package=arduino
 */
 
 #ifndef _VSARDUINO_H_
 #define _VSARDUINO_H_
-#define __AVR_ATmega32u4__
-#define __AVR_ATmega32U4__
+#define __AVR_ATmega328p__
+#define __AVR_ATmega328P__
 #define ARDUINO 105
 #define ARDUINO_MAIN
 #define __AVR__
 #define __avr__
-#define F_CPU 8000000L
+#define F_CPU 16000000L
 #define __cplusplus
 #define __inline__
 #define __asm__(x)
@@ -51,6 +51,26 @@ void rainbow(byte startPosition);
 uint32_t rainbowOrder(byte position);
 
 #include "C:\Program Files (x86)\Arduino\hardware\arduino\cores\arduino\arduino.h"
-#include "C:\Program Files (x86)\Arduino\hardware\SF32u4_boards-master\variants\promicro\pins_arduino.h" 
+#include "C:\Program Files (x86)\Arduino\hardware\arduino\variants\standard\pins_arduino.h" 
 #include "F:\MyRepo\FTCL\FTCL.ino"
+#include "F:\MyRepo\FTCL\FastLED.cpp"
+#include "F:\MyRepo\FTCL\FastLED.h"
+#include "F:\MyRepo\FTCL\FastSPI_LED2.h"
+#include "F:\MyRepo\FTCL\chipsets.h"
+#include "F:\MyRepo\FTCL\clockless.h"
+#include "F:\MyRepo\FTCL\clockless_trinket.h"
+#include "F:\MyRepo\FTCL\controller.h"
+#include "F:\MyRepo\FTCL\delay.h"
+#include "F:\MyRepo\FTCL\dmx.h"
+#include "F:\MyRepo\FTCL\fastpin.h"
+#include "F:\MyRepo\FTCL\fastspi.h"
+#include "F:\MyRepo\FTCL\fastspi_arm.h"
+#include "F:\MyRepo\FTCL\fastspi_avr.h"
+#include "F:\MyRepo\FTCL\fastspi_bitbang.h"
+#include "F:\MyRepo\FTCL\fastspi_dma.h"
+#include "F:\MyRepo\FTCL\hsv2rgb.cpp"
+#include "F:\MyRepo\FTCL\hsv2rgb.h"
+#include "F:\MyRepo\FTCL\lib8tion.cpp"
+#include "F:\MyRepo\FTCL\lib8tion.h"
+#include "F:\MyRepo\FTCL\pixeltypes.h"
 #endif
